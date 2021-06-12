027177dbd488099033fcc0dcdf717adc13fd1df3
Ubuntu <azureuser@monetoring.oufdsnr4qhkurefcgthhkihb5e.bx.internal.cloudapp.net>
2021-06-12 17:33:24 +0000

    code is successfully configuring ansible

--- modules/ec2/instance.tf
+++ modules/ec2/instance.tf
@@ -14,21 +14,29 @@ resource "aws_instance" "ec2_instance" {
 }
 
 
-
 resource "local_file" "inventory"{
     content = "${aws_instance.ec2_instance.public_ip}"
     filename = "/home/azureuser/terraform/terraform-ansible-conf/ansible/playbooks/project1/inventory.ini"
 }
 
+resource "null_resource" "previous" {
 
-resource "null_resource" "execute-ansible"{
-
-    triggers = {
+  triggers = {
     cluster_instance_ids = "${aws_instance.ec2_instance.public_ip}"
     
     }
+}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "10s"
+}
+
+resource "null_resource" "execute-ansible"{
+    depends_on = [time_sleep.wait_30_seconds]
     provisioner "local-exec" {
-        command = "ansible-playbook -i /home/azureuser/terraform/terraform-ansible-conf/ansible/playbooks/project1/inventory.ini  /home/azureuser/terraform/terraform-ansible/ansible/playbooks/project1/master.yml -u ubuntu --private-key ~/.ssh/aws-keypair.pem"
+        command = "sudo ansible-playbook -i /home/azureuser/terraform/terraform-ansible-conf/ansible/playbooks/project1/inventory.ini  /home/azureuser/terraform/terraform-ansible/ansible/playbooks/project1/master.yml -u ubuntu  --private-key ~/.ssh/aws-keypair.pem"
         //command = "ansible-playbook -i ansible/playbooks/project1/inventory.ini  ansible/playbooks/project1/master.yml"
     }
 }
